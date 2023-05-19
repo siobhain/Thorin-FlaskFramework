4210731482c4def4a72c3175e9a6e0937dad0aba
siobhain <>
2023-05-19 17:07:11 +0000

    add flash message on submit of form

--- run.py
+++ run.py
@@ -47,8 +47,8 @@ def about_member(member_name):
 def contact():
     if request.method == "POST":
         print(request.form)
-        # print(request["email"])
-        flash("Thank you {} for subitting".format(request.form.get("name")))
+        print(request.form["email"])
+        flash("Thank you {} for submitting".format(request.form.get("name")))
     return render_template("contact.html", page_title="Contact Us")
 
 
